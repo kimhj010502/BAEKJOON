a36f7b087b4a662e0dfdff4a77cd39ea1768829f
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-06-07 22:25:26 +0900

    Add files via upload

---
+++ 구현/BOJ_10773.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int k, num;
+	cin >> k;
+
+	stack<int> s;
+
+	while (k--) {
+		cin >> num;
+
+		//0인 경우 최근에 쓴 수 지움
+		if (num == 0) {
+			s.pop();
+		}
+		//0이 아닌 경우 해당 수 씀
+		else {
+			s.push(num);
+		}
+	}
+
+	//연산
+	int sum = 0;
+	while (!s.empty()) {
+		sum += s.top();
+		s.pop();
+	}
+
+	//출력
+	cout << sum;
+
+	return 0;
+}
