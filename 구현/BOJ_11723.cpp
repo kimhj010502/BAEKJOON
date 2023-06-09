bd9435247c624ac9b57746c5ce63a6f089b8411b
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-06-09 21:42:47 +0900

    Add files via upload

---
+++ 구현/BOJ_11723.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int m, x;
+	string op;
+	cin >> m;
+
+	set<int> s;
+	set<int> s_all;
+	for (int i = 1; i <= 20; i++) {
+		s_all.insert(i);
+	}
+
+	while (m--) {
+		cin >> op;
+
+		if (op == "add") {
+			cin >> x;
+			s.insert(x);
+		}
+		else if (op == "remove") {
+			cin >> x;
+			s.erase(x);
+		}
+		else if (op == "check") {
+			cin >> x;
+			cout << s.count(x) << "\n";
+		}
+		else if (op == "toggle") {
+			cin >> x;
+
+			//x가 있다면 x 제거
+			if (s.count(x)) {
+				s.erase(x);
+			}
+			//x가 없다면 x 추가
+			else {
+				s.insert(x);
+			}
+		}
+		else if (op == "all") {
+			s = s_all;
+		}
+		else if (op == "empty") {
+			s.clear();
+		}
+	}
+
+	return 0;
+}
