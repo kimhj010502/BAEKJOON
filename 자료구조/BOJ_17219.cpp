b361badf3c46a9161129df3d82f287fd5591cd9a
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-06-21 23:36:34 +0900

    Add files via upload

---
+++ 자료구조/BOJ_17219.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+	string site, pw;
+	cin >> n >> m;
+
+	map<string, string> pw_map;
+	while (n--) {
+		cin >> site >> pw;
+		pw_map.insert({ site, pw });
+	}
+
+	while (m--) {
+		//입력
+		cin >> site;
+
+		//출력
+		cout << pw_map[site] << "\n";
+	}
+
+	return 0;
+}
