19250de9cc4ffca2ff3a88d2061905ba01b538f0
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-10-20 19:30:02 +0900

    Add files via upload

---
+++ 구현/BOJ_2920.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef pair<bool, bool> bb;
+
+string getOrder(vector<int>& number) {
+	bb order = { false, false }; //{ascending 여부, descending 여부}
+
+	for (int i = 1; i < 8; i++) {
+		if (number[i] > number[i - 1]) {
+			order.first = true;
+			continue;
+		}
+		order.second = true;
+	}
+
+	if (order.first && order.second) {
+		return "mixed";
+	}
+	if (order.first) {
+		return "ascending";
+	}
+	return "descending";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	vector<int> number(8);
+	for (int i = 0; i < 8; i++) {
+		cin >> number[i];
+	}
+
+	//연산 & 출력
+	cout << getOrder(number);
+
+	return 0;
+}
